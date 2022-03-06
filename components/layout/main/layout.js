cd3dd00b461e615091bc229086c8b178c6bdd3df
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-03-06 18:55:23 +0200

    Add main layout

---
+++ components/layout/main/layout.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import styles from "../../../styles/layout/main/layout.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <Box component='div' className={styles.container}>
+      <Box>
+        <Box>{children}</Box>
+      </Box>
+    </Box>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Layout from "../components/layout/main/Layout";
+import { Box } from "@mui/material";
+import styles from "../styles/pages/home/home.module.css";
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <Layout>
+      <Box className={styles.container}>Home</Box>
+    </Layout>
+  );
 }
