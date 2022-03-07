095f50f40511db361380d06342ecf170f340685d
augustas augustass <valakeliai@gmail.com>
2022-03-07 22:43:52 +0200

    Prototype of contacts page

--- pages/information.js
+++ pages/information.js
@@ -6,7 +6,10 @@ import styles from "../styles/pages/informationPage/information.module.css";
 export default function Information() {
   return (
     <Layout>
-      <Box className={styles.container}>Informacinis puslapis</Box>
+      <Box className={styles.container}> <p className={styles.contacts1}>Kontaktai</p>
+      <p className={styles.contacts2}>Mindaugas Žiukas - </p>
+      <p className={styles.contacts2}>Matas Grigonis - </p>
+      <p className={styles.contacts2}>Augustas Druceika - </p></Box>
     </Layout>
   );
 }
