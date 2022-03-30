22afa0e79e0ec6e1e9c1d080a80865279c961864
augustas augustass <valakeliai@gmail.com>
2022-03-30 04:09:58 +0300

    Pradetas kurti privatuom politikos puslapis

---
+++ pages/privacy.js
@@ -0,0 +1,102 @@
+//Press ctrl + s to save everytime
+import Layout from "../components/layout/main/Layout";
+import { Box, Divider, List } from "@mui/material";
+import styles from "../styles/pages/informationPage/information.module.css";
+
+export default function Privacy() {
+  return (
+    <Layout>
+        <div className={styles.padding}>
+        <Box className={styles.container} > 
+            <p className={styles.contacts1}>Privatumo politika</p>
+            <p className={styles.contacts3}>
+                Mes suvokiame ypatingą svarbą, tenkančią Jūsų asmens duomenų apsaugai, 
+                todėl renkame ir tvarkome tik tuos Jūsų duomenis, kurie reikalingi 
+                vykdant mūsų veiklą. Asmens duomenis mes tvarkome teisėtai, 
+                skaidriai ir sąžiningai, iš anksto nustatytais tikslais ir 
+                tik tokia apimtimi, kuri reikalinga tikslams pasiekti. 
+                Tvarkydami asmens duomenis, siekiame, kad jie būtų tikslūs, 
+                saugūs, konfidencialūs, tinkamai laikomi ir saugomi.
+            </p>
+                <ol className={styles.olStyle}>
+                Tvarkydami Jūsų asmens duomenis mes laikomės 2016 m. balandžio 27 d. Europos Parlamento ir Tarybos reglamento (ES) 2016/679 dėl fizinių asmenų apsaugos tvarkant asmens duomenis ir dėl laisvo tokių duomenų judėjimo ir kuriuo panaikinama Direktyva 95/46/EB (Bendrasis duomenų apsaugos reglamentas arba BDAR) , Lietuvos Respublikos asmens duomenų teisinės apsaugos įstatymo, taip pat kituose teisės aktuose nustatytų asmens duomenų tvarkymo reikalavimų.
+                </ol>
+                <ol className={styles.olStyle} start="2">
+                Kas mes?
+                    <ul>
+                        <li className={styles.liStyle}>
+                            Mes esame maxSveikata kuri yra max komandos kuriama sveikatos svetaine
+                        </li>
+                        <li>
+                            Remiantis teises aktu nuostatomis, mes esame jusu asmens duomenu valdytojas
+                        </li>
+                        </ul>
+                </ol>
+
+                <ol className={styles.olStyle} start="3">
+                Kas yra sis dokumentas?
+                    <ul>
+                        <li>
+                        Ši privatumo politika (toliau – Privatumo politika) nustato privatumo sąlygas Jums naudojantis interneto svetaine maxSveikata.lt (toliau – Svetainė) taip pat Jums naudojantis teikiamomis sveikatos priežiūros paslaugomis mūsų sveikatos priežiūros įstaigose (toliau gali būti vadinamos klinikomis).
+                        </li>
+                        <li>
+                        Atidžiai perskaitykite šią Privatumo politiką tam, kad suprastumėte, kokią praktiką taikome tvarkydami Jūsų asmens duomenis. Perskaitę šį dokumentą, Jūs sužinosite, kaip mes tvarkome Jūsų asmens duomenis, iš kur juos gauname bei kokios Jūsų kaip asmens duomenų subjekto teisės.
+                        </li>
+                        <li>
+                        Jei naudojatės Svetaine ir perduodate mums savo asmens duomenis siekdami gauti sveikatos priežiūros paslaugas, vadinasi, Jūs perskaitėte ir sutikote su šia Privatumo politika ir joje nurodytais Jūsų asmens duomenų tvarkymo tikslais, būdais bei tvarka. Jeigu Jūs nesutinkate su Privatumo politika, Jūs neturite teisės naudotis Svetaine.
+                        </li>
+                        <li>
+                        Šioje Privatumo politikoje naudojama sąvoka „asmens duomenys“ – tai bet kokia informacija, pagal kurią galima Jus atpažinti – tiesiogiai ar netiesiogiai nustatyti Jūsų tapatybę. Asmens duomenimis yra pavardė, vardas, gimimo data, pašto ar e. pašto adresas, buvimo vietos duomenys ir interneto identifikatorius, Jums būdingos savybės ir kt.
+                        </li>
+                        <li>
+                        Tam, kad Jūs visapusiškai galėtumėte naudotis mūsų teikiamomis paslaugos, Jūs turite būti įrašytas į mūsų klinikos asmenų (pacientų) sąrašus. Tai galite padaryti atvykę į bet kurią mūsų kliniką. Prisirašymo prie mūsų klinikos metu, Jūs būsite supažindintas su mūsų klinikos asmens duomenų tvarkymo taisyklėmis (toliau – Taisyklės), taip pat su Jūsų, kaip paciento ir asmens duomenų subjekto, teisėmis. Tokiu atveju, Jums bus taikomos Taisyklės ir ši Privatumo politika..
+                        </li>
+                        <li>
+                        Privatumo politika galioja visiems asmenims, apsilankiusiems Svetainėje ar apsilankiusiems mūsų klinikoje nepriklausomai nuo to ar Jūs esate mūsų klinikos pacientas, taip pat veiksmams, kuriuos Jūs galite atlikti Svetainėje, įskaitant ir registraciją vizitui pas mūsų klinikos sveikatinimo specialistą
+                        </li>
+                    </ul>
+                </ol>
+                <ol className={styles.olStyle} start="4">
+                    Kokiu principu mes laikomes?
+                    <ul>
+                        <li>
+                        laikysimės galiojančių ir taikomų teisės aktų, tame tarpe ir BDAR, reikalavimų;
+                        </li>
+                        <li>
+                        tvarkysime Jūsų asmens duomenis teisėtu, sąžiningu ir skaidriu būdu;
+                        </li>
+                        <li>
+                        rinksime Jūsų asmens duomenis nustatytais, aiškiai apibrėžtais bei teisėtais tikslais ir toliau netvarkysime su tais tikslais nesuderinamu būdu, išskyrus tiek, kiek tai leidžia teisės aktai;
+                        </li>
+                        <li>
+                        laikysime juos tokia forma, kad Jūsų tapatybę būtų galima nustatyti ne ilgiau, nei tai yra būtina tais tikslais, kuriais asmens duomenys yra tvarkomi;
+                        </li>
+                        <li>
+                        neteiksime asmens duomenų tretiesiems asmenims ir nepaviešinsime jų, išskyrus atvejus, nurodytus Privatumo politikoje ar Taisyklėse (jeigu esate mūsų klinikos pacientu) arba teisės aktų nustatytais atvejais;
+                        </li>
+                        <li>
+                        užtikrinsime, kad Jūsų asmens duomenys būtų tvarkomi tokiu būdu, kad taikant atitinkamas technines ar organizacines priemones būtų užtikrintas tinkamas asmens duomenų saugumas, įskaitant apsaugą nuo duomenų tvarkymo be leidimo arba neteisėto duomenų tvarkymo ir nuo netyčinio praradimo, sunaikinimo ar sugadinimo.
+                        </li>
+                    </ul>
+                </ol>
+                <ol className={styles.olStyle} start="5">
+                    Kokiais tikslais renkame informacija?
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+                </ol>
+                <ol className={styles.olStyle} start="6">
+                Tvarkydami Jūsų asmens duomenis mes laikomės 2016 m. balandžio 27 d. Europos Parlamento ir Tarybos reglamento (ES) 2016/679 dėl fizinių asmenų apsaugos tvarkant asmens duomenis ir dėl laisvo tokių duomenų judėjimo ir kuriuo panaikinama Direktyva 95/46/EB (Bendrasis duomenų apsaugos reglamentas arba BDAR) , Lietuvos Respublikos asmens duomenų teisinės apsaugos įstatymo, taip pat kituose teisės aktuose nustatytų asmens duomenų tvarkymo reikalavimų.
+                </ol>
+
+        </Box>
+            </div>      
+    </Layout>
+  );
+
+
+}
+
+
