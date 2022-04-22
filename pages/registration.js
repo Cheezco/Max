f477e601b631eab8c9f29b02fe0d2cb348f8d6fe
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-04-22 17:21:08 +0300

    Prijungs registracijos backend ir frontend

--- pages/registration.js
+++ pages/registration.js
@@ -2,8 +2,8 @@ import styles from "../styles/pages/registrationPage/registration.module.css";
 import { useRouter } from "next/router";
 import { TextField, Button, Typography, Paper } from "@mui/material";
 import { useState } from "react";
+
 export default function Registration() {
-    
   const [userName, setUserName] = useState("");
   const [name, setName] = useState("");
   const [lastname, setLastname] = useState("");
@@ -12,6 +12,7 @@ export default function Registration() {
   const [doctor, setDoctor] = useState("");
   const [email, setEmail] = useState("");
   const [id, setId] = useState("");
+  const router = useRouter();
 
   const handleUsernameChange = (event) => {
     setUserName(event.target.value);
@@ -37,57 +38,78 @@ export default function Registration() {
   const handleIdChange = (event) => {
     setId(event.target.value);
   };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const obj = {
+      Username: userName,
+      FirstName: name,
+      LastName: lastname,
+      Password: password,
+      Hospital: hospital,
+      Doctor: doctor,
+      Email: email,
+      PersonalCode: id,
+    };
+    const response = await fetch("http://localhost:5000/api/register", {
+      method: "POST",
+      body: JSON.stringify(obj),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      router.push("/login");
+    }
+  };
+
   return (
-    <div className={styles.container}>  
-        <div className={styles.formContainer}>
-            <Paper elevation={3} className={styles.blocks}>
-                <TextField
-                label='Prisijungimo vardas'
-                value={userName}
-                onChange={handleUsernameChange}
-                />
-                <TextField
-                label='Vardas'
-                value={name}
-                onChange={handleNameChange}
-                />
-                <TextField
-                label='Pavardė'
-                value={lastname}
-                onChange={handleLastnameChange}
-                />
-                <TextField
-                label='Slaptažodis'
-                type='password'
-                value={password}
-                onChange={handlePasswordChange}
-                />
-                <TextField
-                label='Ligoninė'
-                value={hospital}
-                onChange={handleHospitalChange}
-                />
-                <TextField
-                label='Gydytojas'
-                value={doctor}
-                onChange={handleDoctorChange}
-                />
-                <TextField
-                label='Elektroninis paštas'
-                type="email"
-                value={email}
-                onChange={handleEmailChange}
-                />
-                <TextField
-                label='Asmens kodas'
-                value={id}
-                onChange={handleIdChange}
-                />
-                <Button variant='outlined'>
-                    <Typography variant='button'>Registruotis</Typography>
-                </Button>
-            </Paper>
-        </div>
+    <div className={styles.container}>
+      <form className={styles.formContainer} onSubmit={handleFormSubmit}>
+        <Paper elevation={3} className={styles.blocks}>
+          <TextField
+            label="Prisijungimo vardas"
+            value={userName}
+            onChange={handleUsernameChange}
+          />
+          <TextField label="Vardas" value={name} onChange={handleNameChange} />
+          <TextField
+            label="Pavardė"
+            value={lastname}
+            onChange={handleLastnameChange}
+          />
+          <TextField
+            label="Slaptažodis"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <TextField
+            label="Ligoninė"
+            value={hospital}
+            onChange={handleHospitalChange}
+          />
+          <TextField
+            label="Gydytojas"
+            value={doctor}
+            onChange={handleDoctorChange}
+          />
+          <TextField
+            label="Elektroninis paštas"
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <TextField
+            label="Asmens kodas"
+            value={id}
+            onChange={handleIdChange}
+          />
+          <Button variant="outlined" type="submit">
+            <Typography variant="button">Registruotis</Typography>
+          </Button>
+        </Paper>
+      </form>
     </div>
   );
 }
